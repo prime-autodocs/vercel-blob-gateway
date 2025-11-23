0c612a335b742898723974f369e560fe1cbdb3dd
Lucas Mendonça <lucas.mendonca@plugify.com.br>
2025-11-23 00:55:41 -0300

    Add delete test

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,4 +1,4 @@
-import { put } from "@vercel/blob";
+import { del, put } from "@vercel/blob";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
@@ -19,4 +19,17 @@ export async function POST(request: Request) {
     url: blob.url,
     pathname: blob.pathname,
   });
+}
+
+export async function DELETE(request: Request) {
+  const data = await request.formData();
+  const pathname = data.get("pathname") as string;
+
+  if (!pathname) {
+    return NextResponse.json({ error: "no pathname" }, { status: 400 });
+  }
+
+  await del(pathname, { token: process.env.BLOB_READ_WRITE_TOKEN });
+
+  return NextResponse.json({ success: true });
 }
